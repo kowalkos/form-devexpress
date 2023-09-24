fdd41dc76f483c934f0959503df0a521cab95345
kowalkos <79119086+kowalkos@users.noreply.github.com>
2023-09-24 14:32:10 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,68 @@
+import { NgModule, Component, enableProdMode } from '@angular/core';
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {
+  DxCheckBoxModule,
+  DxSelectBoxModule,
+  DxNumberBoxModule,
+  DxFormModule,
+} from 'devextreme-angular';
+
+import { Company, Service } from './app.service';
+
+
+@Component({
+  selector: 'demo-app',
+  providers: [Service],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  companies: Company[];
+
+  labelMode: string;
+
+  labelLocation: string;
+
+  readOnly: boolean;
+
+  showColon: boolean;
+
+  minColWidth: number;
+
+  colCount: number;
+
+  width: any;
+
+  constructor(service: Service) {
+    this.labelMode = 'floating';
+    this.labelLocation = 'left';
+    this.readOnly = false;
+    this.showColon = true;
+    this.minColWidth = 300;
+    this.colCount = 2;
+    this.companies = service.getCompanies();
+  }
+
+  getCompanySelectorLabelMode() {
+    return this.labelMode === 'outside'
+      ? 'hidden'
+      : this.labelMode;
+  }
+}
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserTransferStateModule,
+    DxCheckBoxModule,
+    DxSelectBoxModule,
+    DxNumberBoxModule,
+    DxFormModule,
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
